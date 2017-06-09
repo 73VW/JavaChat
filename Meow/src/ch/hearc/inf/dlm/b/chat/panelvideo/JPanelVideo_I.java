5b0e9abac79c45094b2fc37b28e59aabb439ee43
Thibaut <thibaut.piquerez@he-arc.ch>
2017-06-09 09:28:20 +0200

    video

--- Meow/src/ch/hearc/inf/dlm/b/chat/panelvideo/JPanelVideo_I.java
+++ Meow/src/ch/hearc/inf/dlm/b/chat/panelvideo/JPanelVideo_I.java
@@ -5,7 +5,7 @@ import java.awt.image.BufferedImage;
 
 public interface JPanelVideo_I
 	{
-
 	public void setLocalImage(BufferedImage image);
 	public void setExternalImage(BufferedImage image);
+	public void toggleBlackAndWhite();
 	}
